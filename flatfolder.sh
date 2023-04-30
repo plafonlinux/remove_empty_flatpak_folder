6073fb548c0b64bd9603fc2841009b1fe37993ad
plafonlinux <g7platon@gmail.com>
2023-04-30 18:29:12 +0200

    Add bash script to remove empty flatpak folders

---
+++ flatfolder.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Пустая строка для вывода текста
+echo ""
+
+# Получаем список установленных Flatpak приложений
+apps=$(flatpak list --app --columns=application)
+
+# Перебираем папки в директории /home/platon/.var/app
+for folder in /home/plafon/.var/app/*/
+do
+# Получаем имя папки из пути
+folder_name=$(basename $folder)
+
+# Проверяем, присутствует ли имя папки в списке установленных приложений
+if [[ ! $apps =~ $folder_name ]]; then
+echo "Удаляем папку: $folder_name"
+rm -rf "$folder"
+fi
+done
+
+# Выводим сообщение о завершении скрипта
+echo "Готово!"
